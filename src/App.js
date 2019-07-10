d39299ccdf4a01abbc87dd964df721a61855571c
Juicyjames <iceandjames@gmail.com>
2019-07-09 22:06:11 -0600

    snake-dot classname for the body of the snake

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="game-area"></div>
+    <div className="game-area">
+      <div className="snake-dot" style={{ top:0, left:0 }}></div>
+      <div className="snake-dot" style={{ top:0, left:'2%' }}></div>
+      <div className="snake-dot" style={{ top:0, left:'4%' }}></div>
+    </div>
   );
 }
 
