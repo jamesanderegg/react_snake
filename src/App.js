50f7767f09cd05196df2759893de4161b3ad9da0
Juicyjames <iceandjames@gmail.com>
2019-07-09 22:21:28 -0600

    snake body is now stored in state as snakeDots. Made Snake as seperate component, default simple functional component and pass the state of snakeDots as a prop and map through them.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Snake from './Snake'
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="game-area">
-      <div className="snake-dot" style={{ top:0, left:0 }}></div>
-      <div className="snake-dot" style={{ top:0, left:'2%' }}></div>
-      <div className="snake-dot" style={{ top:0, left:'4%' }}></div>
-    </div>
-  );
+  state = {
+    snakeDots: [
+      [0,0],
+      [2,0]
+    ]
+  }
+  render(){
+    return (
+      <div className="game-area">
+        <Snake snakeDots={this.state.snakeDots}/>
+      </div>
+    );
+  }
 }
 
 export default App;
