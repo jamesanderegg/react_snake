75c9488ec0d451e07b811272e8dbccb807d69bd3
Juicyjames <iceandjames@gmail.com>
2019-07-09 22:28:25 -0600

    css for the snake food

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Food from './Food'
 class App extends Component {
 
   state = {
+    food: [6,8],
     snakeDots: [
       [0,0],
       [2,0]
@@ -14,6 +15,7 @@ class App extends Component {
     return (
       <div className="game-area">
         <Snake snakeDots={this.state.snakeDots}/>
+        <Food dot ={this.state.food}/>
       </div>
     );
   }
