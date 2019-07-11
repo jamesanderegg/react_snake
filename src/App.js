480de6b2fef289ac6f8d1cc3758e2f34ccce37b4
Juicyjames <iceandjames@gmail.com>
2019-07-10 18:20:18 -0600

    built function checkIfCollapsed to see if snake has collided with itself.

--- src/App.js
+++ src/App.js
@@ -89,6 +89,17 @@ class App extends Component {
     }
   }
 
+  checkIfCollapsed() {
+    let snake = [...this.state.snakeDots];
+    let head = snake[snake.length - 1];
+    snake.pop();
+    snake.forEach(dot => {
+      if(head[0] == dot[0] && head[1] == dot[1]){
+        this.onGameOver()
+      }
+    })
+
+  }
 
   onGameOver() {
     alert(`Game Over. Snake length is ${this.state.snakeDots.length}`);
